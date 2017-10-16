3f98984869e8385f10868e590c334d1c8ecdc2b3
97starsnavercom <31894223+97starsnavercom@users.noreply.github.com>
2017-10-16 17:58:26 +0900

    Listview 중간쯤 해결

--- app/src/main/java/com/hansung/android/project1/RestaurantDetail.java
+++ app/src/main/java/com/hansung/android/project1/RestaurantDetail.java
@@ -5,13 +5,34 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class RestaurantDetail extends AppCompatActivity {
+    static MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
+
+        // 데이터 원본 준비
+        ArrayList<MyItem> data = new ArrayList<MyItem>();
+        data.add(new MyItem(R.drawable.katsudo, "가츠동", "5000"));
+        data.add(new MyItem(R.drawable.sakedong, "사케동", "7000"));
+        data.add(new MyItem(R.drawable.shrimp, "새우튀김", "6000"));
+        data.add(new MyItem(R.drawable.sushi, "초밥", "15000"));
+
+
+        //어댑터 생성
+        adapter = new MyAdapter(this, R.layout.activity_restaurant_detail, data);
+
+        //어댑터 연결
+        ListView listView = (ListView)findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+
     }
 
     public void callnumber(View view){
