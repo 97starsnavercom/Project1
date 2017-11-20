6baa2ec51747cc594ab4f748d696a3dbed04a8cb
97stars <97stars@naver.com>
2017-11-20 16:38:30 +0900

    Revert "Revert "수정1""
    This reverts commit a48fc0dd18835a4111a1a8502e12d1a982190908.

--- app/src/main/java/com/hansung/android/project1/Registration.java
+++ app/src/main/java/com/hansung/android/project1/Registration.java
@@ -2,78 +2,35 @@ package com.hansung.android.project1;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 
-public class RestaurantDetail extends AppCompatActivity {
-    static MyAdapter adapter;
+    /***********************************************************************************
+    *                                                                                  *
+    *   출처:https://github.com/kwanulee/Android/blob/master/examples/MultimediaTest   *
+    *                                                                                  *
+    ***********************************************************************************/
+
+public class Registration extends AppCompatActivity {
+
 
     private File mPhotoFile =null;
     private String mPhotoFileName = null;
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
-
-    //통화실행코드
-    public void callnumber(View view){
-        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:02-760-4499")));
-    }
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_restaurant);
-
-
-        // 데이터 원본 준비
-        ArrayList<MyItem> data = new ArrayList<MyItem>();
-        data.add(new MyItem(R.drawable.katsudo, "가츠동", "5000","3.1"));
-        data.add(new MyItem(R.drawable.sakedong, "사케동", "7000","4.7"));
-        data.add(new MyItem(R.drawable.shrimp, "새우튀김", "6000","2.2"));
-        data.add(new MyItem(R.drawable.sushi, "초밥", "15000","3.4"));
-
-
-        //어댑터 생성
-        adapter = new MyAdapter(this, R.layout.item, data);
-
-        //어댑터 연결
-        ListView listView = (ListView)findViewById(R.id.listview);
-        listView.setAdapter(adapter);
-
-        //Listner 부착
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View vClicked, int position, long id) {
-
-                String name = ((MyItem)adapter.getItem(position)).nName;
-                int Icon=((MyItem)adapter.getItem(position)).mIcon;
-                String Price=((MyItem)adapter.getItem(position)).nPrice;
-                String Score=((MyItem)adapter.getItem(position)).nScore;
-
-                //MenuDetail에 속성값 전달
-                Intent intent=new Intent(getApplicationContext(),MenuDetail.class);
-                intent.putExtra("Option1",name);
-                intent.putExtra("Option2",Icon);
-                intent.putExtra("Option3",Price);
-                intent.putExtra("Option4",Score);
-                startActivity(intent);
-
-            }
-        });
     }
 
     public void restaurant_image(View view){
@@ -102,5 +59,6 @@ public class RestaurantDetail extends AppCompatActivity {
         String  currentTimeStamp = dateFormat.format(new Date());
         return currentTimeStamp;
     }
+
 
 }
