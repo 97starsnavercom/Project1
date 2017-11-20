a48fc0dd18835a4111a1a8502e12d1a982190908
97stars <97stars@naver.com>
2017-11-20 16:38:22 +0900

    Revert "수정1"
    This reverts commit c1706c1fd383c6d5c8fcf583c28122505bdcff3a.

--- app/src/main/java/com/hansung/android/project1/RestaurantDetail.java
+++ app/src/main/java/com/hansung/android/project1/RestaurantDetail.java
@@ -2,8 +2,6 @@ package com.hansung.android.project1;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,30 +12,20 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class RestaurantDetail extends AppCompatActivity {
     static MyAdapter adapter;
-
-    private File mPhotoFile =null;
-    private String mPhotoFileName = null;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-
 
     //통화실행코드
     public void callnumber(View view){
         startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:02-760-4499")));
     }
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registration_restaurant);
+        setContentView(R.layout.activity_restaurant_detail);
 
 
         // 데이터 원본 준비
@@ -74,33 +62,6 @@ public class RestaurantDetail extends AppCompatActivity {
 
             }
         });
-    }
-
-    public void restaurant_image(View view){
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            //1. 카메라 앱으로 찍은 이미지를 저장할 파일 객체 생성
-            mPhotoFileName = "IMG"+currentDateFormat()+".jpg";
-            mPhotoFile = new File("data/data/com.hansung.android.project1/files/", mPhotoFileName);
-
-            if (mPhotoFile !=null) {
-                //2. 생성된 파일 객체에 대한 Uri 객체를 얻기
-                Uri imageUri = FileProvider.getUriForFile(this, "com.example.kwanwoo.multimediatest", mPhotoFile);
-
-                //3. Uri 객체를 Extras를 통해 카메라 앱으로 전달
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            } else
-                Toast.makeText(getApplicationContext(), "file null", Toast.LENGTH_SHORT).show();
-        }
-
-    }
-
-    private String currentDateFormat(){
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HH_mm_ss");
-        String  currentTimeStamp = dateFormat.format(new Date());
-        return currentTimeStamp;
     }
 
 }
